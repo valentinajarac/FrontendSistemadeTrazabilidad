6771a3f1e6296b03a3148bd234b5d2612c875382
Valentinajarac <valentina_jaramillo82181@elpoli.edu.co>
2024-11-28 13:22:12 -0500

    conexiones con back

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,7 @@
+const API_URL = 'https://sistemawebdetrazabilidad-e19da3b0d7eb.herokuapp.com/api';
+
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || 'https://sistemawebdetrazabilidad-e19da3b0d7eb.herokuapp.com/api',
+  BASE_URL: API_URL,
   TIMEOUT: 10000,
   AUTH: {
     TOKEN_KEY: 'trazafrutas_token',
